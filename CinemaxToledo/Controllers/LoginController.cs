ab3c6ca0de7fc36d5fb5939e89d6c8ab923c897d
Felipe Sanches <felipesteam2010@gmail.com>
2024-09-05 22:04:47 -0300

    commit ana paula

--- CinemaxToledo/Controllers/LoginController.cs
+++ CinemaxToledo/Controllers/LoginController.cs
@@ -127,14 +127,14 @@ namespace CompusoftAtendimento.Controllers
                 HttpContext.Session.SetInt32("idLogin", model.id );
                 HttpContext.Session.SetString("nomeLogin", model.login);
                 HttpContext.Session.SetInt32("ativoTrue", model.ativo == true?1:0);
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("cadastro", "Atendimento");
             }
             else
             {
                 HttpContext.Session.SetInt32("idLogin", model.id);
                 HttpContext.Session.SetString("nomeLogin", model.login);
                 HttpContext.Session.SetInt32("ativoTrue", model.ativo == false ? 1 : 0);
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("cadastro", "Atendimento");
             }
         }
 
