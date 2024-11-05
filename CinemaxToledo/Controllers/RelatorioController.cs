7de9a7afc3e571e7ddf24d3fb26bf5af6c23f944
Felipe Sanches <felipesteam2010@gmail.com>
2024-11-05 19:34:38 -0300

    front relatorios

---
+++ CinemaxToledo/Controllers/RelatorioController.cs
@@ -0,0 +1,34 @@
+﻿using CompusoftAtendimento.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CompusoftAtendimento.Controllers
+{
+        public class RelatorioController : Controller
+    {
+            public IActionResult menu()
+            {
+                return View(); //passando a lista por parametro para a view
+            }
+            public IActionResult agentes()
+            {
+                return View(); //passando a lista por parametro para a view
+            }
+            public IActionResult categoriadoproblema()
+            {
+                return View(); //passando a lista por parametro para a view
+            }
+            public IActionResult plataformas()
+            {
+                return View(); //passando a lista por parametro para a view
+            }
+            public IActionResult empresas()
+            {
+                return View(); //passando a lista por parametro para a view
+            }
+            public IActionResult formasdeatendimento()
+            {
+                return View(); //passando a lista por parametro para a view
+            }
+    }
+}
